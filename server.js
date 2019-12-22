3a803b1aaf744f08b24d3f38da1cef10c38216e1
vijay-mca <bpvijay97@gmail.com>
2019-12-22 15:53:45 +0530

    database connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8000;
+
+mongoose.Promise.global;
+const uri = process.env.ATLAS_URI || 'mongodb+srv://vijay:vijay-mongo@cluster0-x42br.gcp.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true});
+const connection = mongoose.connection;
+connection.on('error',console.error.bind(console,'connextion error'))
+connection.once('open',() => {
+    console.log('connection established');
+});
+
 app.listen(PORT,() => {
     console.log(`Node Servr Running On PORT: ${PORT}`);
 });
